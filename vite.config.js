8ae046fa5536da29bbfba044462a1e0515f8afb7
RodrigoSierraV <rosierrav@yahoo.com>
2024-01-03 13:10:38 +0000

    Add docker and react query

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
 })
