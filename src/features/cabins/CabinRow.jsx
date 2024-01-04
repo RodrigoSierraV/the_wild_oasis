f54f282324ab851e3bb3cb77518bacf6418f57a5
RodrigoSierraV <rosierrav@yahoo.com>
2024-01-04 14:01:57 +0000

    Add mutation

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import {formatCurrency} from "../../utils/helpers"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -42,12 +44,25 @@ const Discount = styled.div`
 
 function CabinRow({ cabin }) {
   const {
+    id: cabinId,
     name,
     maxCapacity,
     regularPrice,
     discount,
     image
   } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: err => alert(err.message),
+  });
 
   return (
     <TableRow role="row">
@@ -56,7 +71,7 @@ function CabinRow({ cabin }) {
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>Delete</button>
     </TableRow>
   )
 }
