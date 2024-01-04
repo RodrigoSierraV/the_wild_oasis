c3a26d248111f6aabb09dc7bec71f5d0f14e34f4
RodrigoSierraV <rosierrav@yahoo.com>
2024-01-04 13:27:02 +0000

    Add cabins table

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
 
@@ -9,11 +10,16 @@ function Cabins() {
     getCabins().then((data) => console.log(data));
   }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://trvlmfyyoaqxedhfnekm.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" alt="cab-1" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
